e03ca575284e1a09894dcaeb130608843b8b9b06
Maximiliano Albarran <maxialbarran@hotmail.com>
2022-01-19 16:45:52 -0300

    Maquetado y estilizado de Comic Page finalizado

--- src/components/comicsCard/index.jsx
+++ src/components/comicsCard/index.jsx
@@ -1,27 +1,32 @@
 import React from 'react';
 import '../../styles/comics.css';
 
-const ComicsCard = () => {
+const ComicsCard = ({ comic }) => {
+
+  const isDescription = (text, title) => {
+    if (text === null) {
+      return <h1>{title}</h1>;
+    } else {
+      return text;
+    }
+  };
+
   return (
     <div className='comic-card'>
       <div className='cover-comic-card'>
         <img
-          src='http://i.annihil.us/u/prod/marvel/i/mg/b/80/4f206cc0ac28a.jpg'
+          src={comic.thumbnail.path + '.' + comic.thumbnail.extension}
           alt='Imagen del comic'
           className='cover-image'
         />
       </div>
       <div className='presentation-comic-card'>
         <div className='presentation-header'>
-          <h3 className='header-title'>Deadpool (1997) #44</h3>
+          <h3 className='header-title'>{comic.title}</h3>
         </div>
         <div className='presentation-section'>
           <p className='section-description'>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Error
-            officia aperiam placeat quis odit iusto nesciunt asperiores enim
-            soluta laudantium rem hic maiores porro, aspernatur totam, eaque ea
-            a ducimus illum. Iure, voluptatibus at quae beatae cum maxime
-            voluptate consequuntur.
+            {isDescription(comic.description, comic.title)}
           </p>
         </div>
       </div>
--- src/pages/Comics.jsx
+++ src/pages/Comics.jsx
@@ -1,38 +1,43 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import ComicsCard from '../components/comicsCard';
 
-
 const Comics = (props) => {
-
   const params = useParams();
 
-  const [comics, setComics] = useState([])
-  
-  useEffect(()=>{
-  const getComics = async () => {
-    try {
-      const document = await fetch(
-        `https://gateway.marvel.com:443/v1/public/characters/${params.id}/comics?apikey=7489a7d4c2fd1b00acad64c61aa4355d`
-      );
-      const response = await document.json();
-      setComics(response.data.results);
-    } catch (e) {
-      console.log(e);
-    }
-  };
-  getComics();
-}, []);
+  const [comics, setComics] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-  console.log(comics)
+  useEffect(() => {
+    const getComics = async () => {
+      try {
+        const document = await fetch(
+          `https://gateway.marvel.com:443/v1/public/characters/${params.id}/comics?apikey=7489a7d4c2fd1b00acad64c61aa4355d`
+        );
+        const response = await document.json();
+        setComics(response.data.results);
+        setLoading(false);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    getComics();
+  }, []);
 
-  return(
-    <div>
-      <div className="comics-layout">
-        <ComicsCard />
+  console.log(comics);
+
+  if (loading) return <div>Loading...</div>;
+  else {
+    return (
+      <div>
+        <div className='comics-layout'>
+          {comics.map((comic) => (
+            <ComicsCard comic={comic} key={comic.id} />
+          ))}
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 };
 
 export default Comics;
