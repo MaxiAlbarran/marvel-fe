c7504ae7b47ba0e58abcda582f892ee279219803
Maximiliano Albarran <maxialbarran@hotmail.com>
2022-01-20 20:53:14 -0300

    Avengers page con busqueda funcional

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from './pages/Home';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import Comics from './pages/Comics';
 import Navbar from './components/navbar';
+import Avengers from './pages/Avengers';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Home exact/>}/>
           <Route path='/comics/:id' element={<Comics exact/>}/>
+          <Route path='/avengers/' element={<Avengers exact/>}/>
         </Routes>
       </Router>
     </div>
