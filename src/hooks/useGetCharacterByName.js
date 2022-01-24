74f74a534b12caa63dd18454196d911784ebded6
Maximiliano Albarran <maxialbarran@hotmail.com>
2022-01-24 16:03:13 -0300

    Se agrega un controlador de refresh

--- src/hooks/useGetCharacterByName.js
+++ src/hooks/useGetCharacterByName.js
@@ -2,21 +2,23 @@ import React, { useState, useEffect } from 'react';
 
 const useGetCharacterByName = (name) => {
   const [character, setCharacter] = useState([]);
+  const [reload, setReload] = useState(false)
 
   const searchChars = async () => {
     setCharacter([]);
+    setReload(true)
     try {
       const document = await fetch(
         `https://gateway.marvel.com:443/v1/public/characters?name=${name}&apikey=7489a7d4c2fd1b00acad64c61aa4355d`
       );
       const response = await document.json();
       setCharacter(response.data.results);
-
+      setReload(false)
     } catch (e) {
       console.log(e);
     }
   };
-  return [character, searchChars];
+  return [character, searchChars, reload];
 };
 
 export default useGetCharacterByName;
