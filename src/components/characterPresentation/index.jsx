c271eff77a8c415b86f74e3e3352ef9beec8eaf8
Maximiliano Albarran <maxialbarran@hotmail.com>
2022-01-25 10:25:59 -0300

    Presentacion de personaje maquetada y estilizada

--- src/components/characterPresentation/index.jsx
+++ src/components/characterPresentation/index.jsx
@@ -1,27 +1,22 @@
 import React from 'react';
 import '../../styles/presentation.css';
 
-const CharacterPresentation = () => {
+const CharacterPresentation = ({figure}) => {
   return (
     <div className='presentation-container'>
       <div className="header">
-        <h4 className="title-header">3-D Man</h4>
+        <h4 className="title-header">{figure.name}</h4>
       </div>
       <div className='side-image'>
         <img
-          src='http://i.annihil.us/u/prod/marvel/i/mg/c/e0/535fecbbb9784.jpg'
+          src={figure.thumbnail.path + '.jpg'}
           alt='Character image'
           className='image'
         />
       </div>
       <div className='body-description'>
         <p className='description'>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque
-          voluptatum ipsa voluptate veniam magni et dolor nam quae? Odio,
-          perferendis. Non, incidunt quo nesciunt vero, neque omnis nihil vitae
-          odit facilis expedita magni repellendus magnam doloremque ullam
-          possimus nam totam iure animi, aliquid tempora aut porro minima.
-          Obcaecati, sequi nemo.
+          {figure.description}
         </p>
       </div>
     </div>
--- src/pages/Character.jsx
+++ src/pages/Character.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import useShowCharacterById from '../hooks/useShowCharacterById';
 import { useParams } from 'react-router-dom';
 import CharactersCard from '../components/charactersCard';
+import '../styles/presentation.css'
 import CharacterPresentation from '../components/characterPresentation';
 
 const Character = () => {
@@ -10,10 +11,13 @@ const Character = () => {
 
   const [character, loading] = useShowCharacterById(params)
 
+  console.log(character)
+
 
   return(
-    <div>
-      <CharacterPresentation />
+    <div className='presentation-layout'>
+      {character.map(item=>
+        <CharacterPresentation figure={item} key={item.id}/>)}
     </div>
   );
 };
