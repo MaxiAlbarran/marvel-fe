e751cbad26463d10458ec3c5749ab980da65fee8
Maximiliano Albarran <maxialbarran@hotmail.com>
2022-01-25 12:29:09 -0300

    Añadicion de nuevas rutas

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import Home from './pages/Home';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
-import Comics from './pages/Comics';
+import Comics from './pages/Characters/Comics';
 import Navbar from './components/navbar';
-import Character from './pages/Character';
-import Search from './pages/Search';
-import Events from './pages/Events';
+import Character from './pages/Characters/Character';
+import Search from './pages/Characters/Search';
+import Events from './pages/Event/Events';
+import CharactersEvent from './pages/Event/CharactersEvent';
 
 function App() {
   return (
@@ -14,10 +15,13 @@ function App() {
       <Navbar />
         <Routes>
           <Route path='/' element={<Home exact/>}/>
-          <Route path='/comics/:id' element={<Comics exact/>}/>
-          <Route path='/character/:id' element={<Character exact/>}/>
+
           <Route path='/searchCharacters/' element={<Search exact/>}/>
+          <Route path='/character/:id' element={<Character exact/>}/>
+          <Route path='/comics/:id' element={<Comics exact/>}/>
+
           <Route path='/searchEvents/' element={<Events exact/>}/>
+          <Route path='/charactersEvent/:id' element={<CharactersEvent exact/>}/>
         </Routes>
       </Router>
     </div>
