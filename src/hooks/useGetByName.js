bf10aaede12d8bebca42af684368323b7ba78938
Maximiliano Albarran <maxialbarran@hotmail.com>
2022-01-25 16:31:48 -0300

    Estructuracion del hook

--- src/hooks/useGetByName.js
+++ src/hooks/useGetByName.js
@@ -1,19 +1,17 @@
-import React, { useState, useEffect } from 'react';
+import { useState } from 'react';
 
-const useGetCharacterByName = (name) => {
+const useGetByName = (URL) => {
   const [character, setCharacter] = useState([]);
-  const [reload, setReload] = useState(false)
+  const [reload, setReload] = useState(false);
 
   const searchChars = async () => {
     setCharacter([]);
-    setReload(true)
+    setReload(true);
     try {
-      const document = await fetch(
-        `https://gateway.marvel.com:443/v1/public/characters?nameStartsWith=${name}&apikey=7489a7d4c2fd1b00acad64c61aa4355d`
-      );
+      const document = await fetch(URL);
       const response = await document.json();
       setCharacter(response.data.results);
-      setReload(false)
+      setReload(false);
     } catch (e) {
       console.log(e);
     }
@@ -21,4 +19,4 @@ const useGetCharacterByName = (name) => {
   return [character, searchChars, reload];
 };
 
-export default useGetCharacterByName;
+export default useGetByName;
